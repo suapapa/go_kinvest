6a4df1e8bb35952e900c6fe55152ea1cd0bd8b69
Homin Lee <ff4500@gmail.com>
2025-04-19 16:58:51 +0900

    feat: add example for get holdings

---
+++ examples/get_holdings/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+
+	"github.com/goccy/go-yaml"
+	kinvest "github.com/suapapa/go_kinvest"
+)
+
+func main() {
+	kc, err := kinvest.NewClient(nil)
+	if err != nil {
+		panic(err)
+	}
+
+	res, err := kc.GetDomesticHoldings(context.Background(), nil)
+	if err != nil {
+		// for unwrappedErr := err; unwrappedErr != nil; unwrappedErr = errors.Unwrap(unwrappedErr) {
+		// 	fmt.Println("Error:", unwrappedErr)
+		// }
+		panic(err)
+	}
+	y, err := yaml.Marshal(res)
+	if err != nil {
+		panic(err)
+	}
+	println(string(y))
+}
